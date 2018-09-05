5e965c15a64680a81852c6df947ebc9f542eb95b
jacob <jacob@jacobs-MacBook-Air.local>
2018-09-05 14:33:40 -0400

    another commit

--- src/components/About.js
+++ src/components/About.js
@@ -11,9 +11,9 @@ const About = () => (
 </div>
 
 <div className="about_content">
-<p>After university, I spent a few years working in SaaS sales. Rather than just selling digital products, I want to know how sofware works as well. A combination of self-study and online community learning helped me become a field engineer at an autonoumous guided vehicle company based in Boston where I traveled America's factories to program (C++) self driving vehicles.</p>
+<p>After university, I spent a few years working in SaaS sales. Rather than just selling digital products, I wanted to know how sofware works as well. A combination of self-study, online, and in person learning helped me become a field engineer at an autonoumous guided vehicle company based in Boston where I traveled America's factories to program (C++) self driving vehicles.</p>
 
-<p>I recently returned to New York to seek additional software engineering training and work (less weekly cross country flights :) ).</p>
+<p>I recently returned to New York to seek additional software engineering training and work.</p>
 
 <p>In my freetime, I can likely be found working on a personal coding project, playing pickup basketball, or cycling around New York City.</p>
 </div>
