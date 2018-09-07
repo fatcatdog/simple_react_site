925de0aae7b21e59cf4147c967bb64c356a2fbef
jacob <jacob@jacobs-MacBook-Air.local>
2018-09-07 13:36:33 -0400

    add react helmet

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import Header from './Header';
 import Main from './Main';
 import '../css/App.css';
+import {Helmet} from "react-helmet";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 library.add(fab)
 
 const App = () => (
   <div className="everything">
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Jacob Duchen</title>
+      <link rel="canonical" href="https://fatcatdog.github.io/" />
+    </Helmet>
     <Header />
     <Main />
   </div>
