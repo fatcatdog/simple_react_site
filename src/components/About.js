40a73096ba170f8e7ef3616df0f12023f43348e8
jacob <jacob@jacobs-MacBook-Air.local>
2018-09-10 14:44:46 -0400

    changing about me section

--- src/components/About.js
+++ src/components/About.js
@@ -11,11 +11,11 @@ const About = () => (
 </div>
 
 <div className="about_content">
-<p>After university, I spent a few years working in SaaS sales. Rather than just selling digital products, I wanted to know how sofware works as well. A combination of self-study, online, and in person learning helped me become a field engineer at an autonoumous guided vehicle company based in Boston where I traveled America's factories to program (C++) self driving vehicles.</p>
+<p>After working in SaaS sales for a few years I realized I wanted to learn how to build digital products.</p>
 
 <p>I recently returned to New York to seek additional software engineering training and work.</p>
 
-<p>In my freetime, I can likely be found working on a personal coding project, playing pickup basketball, or cycling around New York City.</p>
+<p>In my freetime, I can likely be found coding, playing pickup basketball, or cycling around New York City.</p>
 </div>
 
 </div>
